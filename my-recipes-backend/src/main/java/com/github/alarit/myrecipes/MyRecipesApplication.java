49556c557591d692eb1dea37280c8a76285292b5
Alessandro Arici <a.arici@techgap.it>
2017-09-18 17:45:11 +0200

    Updated pom

--- my-recipes-backend/src/main/java/com/github/alarit/myrecipes/MyRecipesApplication.java
+++ my-recipes-backend/src/main/java/com/github/alarit/myrecipes/MyRecipesApplication.java
@@ -2,11 +2,18 @@ package com.github.alarit.myrecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyRecipesApplication {
+public class MyRecipesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRecipesApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(MyRecipesApplication.class);
 	}
 }
