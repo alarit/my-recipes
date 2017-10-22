d62ad6211df6275eb214baebe8053772e4ce9ea4
Alessandro Arici <a.arici@techgap.it>
2017-10-22 18:27:19 +0200

    Removed external tomcat and cleaned properties

--- my-recipes-backend/src/main/java/com/github/alarit/myrecipes/MyRecipesApplication.java
+++ my-recipes-backend/src/main/java/com/github/alarit/myrecipes/MyRecipesApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyRecipesApplication extends SpringBootServletInitializer {
+public class MyRecipesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRecipesApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
-		return app.sources(MyRecipesApplication.class);
 	}
 }
